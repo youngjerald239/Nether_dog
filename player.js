5b718ef899e7a225dcbba7a1f1b9f16665350a85
youngjerald239 <youngjerald239@gmail.com>
2022-09-03 17:23:12 -0500

    added running and jumping states

--- player.js
+++ player.js
@@ -1,4 +1,4 @@
-import {Sitting, Running} from './playerStates.js'
+import {Sitting, Running, Jumping, Falling} from './playerStates.js'
 
 export class Player {
     constructor(game){
@@ -14,7 +14,7 @@ export class Player {
         this.frameY = 0
         this.speed = 0
         this.maxSpeed = 10
-        this.states = [new Sitting(this), new Running(this)]
+        this.states = [new Sitting(this), new Running(this), new Jumping(this), new Falling(this)]
         this.currentState = this.states[0]
         this.currentState.enter()
     }
@@ -28,7 +28,7 @@ export class Player {
         if (this.x < 0) this.x = 0
         if (this.x > this.game.width - this.width) this.x = this.game.width - this.width
         // vertical movement
-        if (input.includes('ArrowUp') && this.onGround()) this.vy -= 30
+        // if (input.includes('ArrowUp') && this.onGround()) this.vy -= 30
         this.y += this.vy
         if (!this.onGround()) this.vy += this.weight
         else this.vy = 0
