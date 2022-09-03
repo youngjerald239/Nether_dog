ae7c445d7e71e88e5b339fe1d519b834154132c5
youngjerald239 <youngjerald239@gmail.com>
2022-09-03 15:38:28 -0500

    created canvas and player rect

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
+import {Player} from './player.js'
+
 window.addEventListener('load', function(){
     const canvas = document.getElementById('canvas1')
     const ctx = canvas.getContext('2d')
     canvas.width = 500
     canvas.height = 500
+
+    class Game {
+        constructor(width, height) {
+            this.width = width
+            this.height = height
+            this.player = new Player(this)
+        }
+        update(){
+
+        }
+        draw(context){
+            this.player.draw(context)
+        }
+    }
+
+    const game = new Game(canvas.width, canvas.height)
+    console.log(game)
+
+    function animate(){
+        game.draw(ctx)
+        requestAnimationFrame(animate)
+    }
+    animate()
 })
