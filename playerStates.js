0b636abb7bea1b5b2c3e4aef12ce1f85ee3a1126
youngjerald239 <youngjerald239@gmail.com>
2022-09-03 23:33:50 -0500

    added roll and diving

--- playerStates.js
+++ playerStates.js
@@ -1,4 +1,4 @@
-import {Dust, Fire} from './particles.js'
+import {Dust, Fire, Splash} from './particles.js'
 
 const states = {
     SITTING: 0,
@@ -136,8 +136,32 @@ export class Diving extends State {
         this.game.particles.unshift(new Fire(this.game, this.game.player.x + this.game.player.width * 0.5, this.game.player.y + this.game.player.height * 0.5))
         if (this.game.player.onGround()) {
             this.game.player.setState(states.RUNNING, 1)
+            for (let i = 0; i < 30; i++){
+                this.game.particles.unshift(new Splash(this.game, this.game.player.x, this.game.player.y))
+            }
         } else if (input.includes('Enter') && this.game.player.onGround()) {
             this.game.player.setState(states.ROLLING, 2)
+        } 
+    
+    }    
+}
+
+export class Hit extends State {
+    constructor(game) {
+        super('HIT', game)
+    } 
+    enter(){
+        if(this.game.player.onGround()) this.game.player.vy -= 30
+        this.game.player.frameX = 0
+        this.game.player.maxFrame = 10
+        this.game.player.frameY = 4
+   
+    }
+    handleInput(input) {
+        if (this.game.player.frameX >= 10 && this.game.player.onGround()) {
+            this.game.player.setState(states.RUNNING, 1)
+        } else if (this.game.player.frameX >= 10 && !this.game.player.onGround()) {
+            this.game.player.setState(states.FALLING, 1)
         } 
     
     }    
